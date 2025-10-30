3c7f2fb32479c06c59aab3847c2de01e226663e5
Exstee <87457548+Exstee@users.noreply.github.com>
2025-10-30 23:19:17 +0000

    feat: add Passport serialize and deserialize user setup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
 const passport = require('passport');
+const { ObjectID } = require('mongodb');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
@@ -29,18 +30,36 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Set up the view engine
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
+
 fccTesting(app); // For fCC testing purposes
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serialization / Deserialization logic
+passport.serializeUser((user, done) => {
+  done(null, user._id); // store the user’s _id in the session
+});
+
+passport.deserializeUser((id, done) => {
+  // Will later fetch the full user from DB by ID.
+  // Commented out for now until DB is ready:
+  // myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  //   done(null, doc);
+  // });
+  done(null, null);
+});
+
+// Routes
 app.route('/').get((req, res) => {
   res.render('index', { title: 'Hello', message: 'Please log in' });
 });
 
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
